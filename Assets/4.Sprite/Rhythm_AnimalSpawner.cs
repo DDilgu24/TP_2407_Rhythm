cfc5a8afde60ade71acc43055f1e48017f298f4f
조현준 <chj97610@gmail.com>
2024-07-09 09:58:52 +0900

    Day 2 - 체크포인트 1
    리듬은 맞춤

--- Assets/4.Sprite/Rhythm_AnimalSpawner.cs
+++ Assets/4.Sprite/Rhythm_AnimalSpawner.cs
@@ -9,15 +9,30 @@ public class Rhythm_AnimalSpawner : MonoBehaviour
     // public Rhythm_AnimalPooling animalPool;
     [SerializeField] private Transform Spawner;
     private GameObject obj;
-    private double current_time = -3.125d;
+    private double current_time = -3.225d;
     private int BPM = 130;
     private int count = 0;
-    private bool[] animal_appear;
+    private int[] animal_appear;
 
     private void Start()
     {
-        // animal_appear = new bool[111];
+        animal_appear = new int[112]
+        {
+            0,0,1,0,0,0,0,0,0,0,
+            1,0,0,0,0,0,0,0,1,0,
+            0,0,1,0,0,0,1,0,0,1,
+            1,0,0,0,1,0,0,0,0,0,
+            0,0,1,0,0,0,0,0,0,0,
 
+            1,0,0,0,1,0,0,0,1,0,
+            0,0,1,0,0,0,1,0,0,0,
+            1,0,0,0,1,0,0,1,1,0,
+            0,0,1,0,0,0,1,0,0,0,
+            1,0,0,0,1,1,0,0,1,0,
+
+            0,0,1,1,0,0,1,0,0,0,
+            1,1
+        };
     }
 
     private void FixedUpdate()
@@ -26,15 +41,21 @@ public class Rhythm_AnimalSpawner : MonoBehaviour
         current_time += Time.deltaTime;
         if (current_time > 60d / BPM)
         {
-            // 동물 생성하는 부분
-            // 1. 동물을 풀에서 꺼내온다.
-            obj = Rhythm_AnimalPooling.instance.GetObjectFromPool();
-            // 2. 풀에서 꺼낸 동물의 위치를 설정한다.
-            obj.transform.position = Spawner.position;
-            // 3. Rigidbody에 힘 적용(방향 - forward / 크기 / 힘의 종류)
-            Vector3 v = new Vector3(-0.5f, 0.5f, -0.7f);
-            obj.GetComponent<Rigidbody>().AddForce(v * 13f, ForceMode.Impulse);
-
+            if(animal_appear[count] < 1)
+            {
+                // 안 나오는 박자
+            }
+            else
+            {
+                // 동물 생성하는 부분
+                // 1. 동물을 풀에서 꺼내온다.
+                obj = Rhythm_AnimalPooling.instance.GetObjectFromPool();
+                // 2. 풀에서 꺼낸 동물의 위치를 설정한다.
+                obj.transform.position = Spawner.position;
+                // 3. Rigidbody에 힘 적용(방향 - forward / 크기 / 힘의 종류)
+                Vector3 v = new Vector3(-0.5f, 0.5f, -0.7f);
+                obj.GetComponent<Rigidbody>().AddForce(v * 13f, ForceMode.Impulse);
+            }
             count++;
             current_time -= (60d / BPM);
         }
